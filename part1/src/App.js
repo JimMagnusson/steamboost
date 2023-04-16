e286d630580dea28062630409a38de544e4c3f0a
Jim Magnusson <jim.magnusson97@gmail.com>
2023-04-16 15:35:17 +0200

    Add some array handling and conditional rendering

--- part1/src/App.js
+++ part1/src/App.js
@@ -48,11 +48,26 @@ const Hello = ({name, age}) => {
   )
 }
 
-// For 1b
+// For 1c
 const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text} </button>
 
+// For 1d
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
 const App = (props) => {
 
@@ -86,25 +101,32 @@ const App = (props) => {
   */
 
   // 1d More complex state
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1})
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
-  const handleRightClick = () => 
-  {
-    setClicks({ ...clicks, right: clicks.right + 1})
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRigth = right + 1
+    setRight(updatedRigth)
+    setTotal(left + updatedRigth)
   }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
