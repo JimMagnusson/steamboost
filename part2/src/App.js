73ecdb54b071e58546a1c1088785af1509949359
Jim Magnusson <jim.magnusson97@gmail.com>
2023-04-18 12:28:15 +0200

    Add getting data from server example

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,18 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from 'react'
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import Filter from "./components/Filter"
+import axios from "axios"
 
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -51,6 +47,17 @@ const App = () => {
   const personsToShow = (filter === '')
     ? persons
     : persons.filter(word => word.name.toLowerCase().indexOf(filter) > -1)
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
