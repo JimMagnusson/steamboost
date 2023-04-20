3904a9d1d18df257f17917039b81d7318edfa85c
Jim Magnusson <jim.magnusson97@gmail.com>
2023-04-20 21:07:47 +0200

    Add endpoint in backend for short description generation using ChatGPT

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
 const app = express()
 
 app.use(express.json())
@@ -8,6 +17,18 @@ app.use(cors())
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
+
+app.post("/generate-short-description", async (request, response) => {
+    const body = request.body
+    const promt = "Can you give a suggestion for a short description for a Steam store page where the game is about " + body.description
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "user", content: promt}
+      ]
+    });
+    response.send(completion.data.choices[0].message.content);
+});
 
 const PORT = 3001
 app.listen(PORT)
