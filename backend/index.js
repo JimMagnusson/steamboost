e6377580c65f889e3370d7de56d33e5a2e085f7e
Jim Magnusson <jim.magnusson97@gmail.com>
2023-05-15 11:53:22 +0200

    Implement connection between backend and database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -14,8 +15,64 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+// Schema for the database
+const gameSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+})
+
+/*
+gameSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+*/
+const Game = mongoose.model('Game', gameSchema)
+
+if (process.argv.length<3 ) {
+  console.log('give password')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://jimmagnusson:${password}@cluster0.qw1m2ir.mongodb.net/gameComparisonApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
+})
+
+app.post('/set-steam-games', async (request, response) => {
+  const body = request.body
+
+  response.send('<h1>Set Steam Apps</h1>')
+})
+
+
+app.get('/get-steam-games', (request, response) => {
+  Game
+  .find({})
+  .then(games => {
+    response.json(games)
+    mongoose.connection.close()
+  })
+})
+
+app.post('/add-steam-games', async (request, response) => {
+  const game = new Game({
+    id: 0,
+    name: 'Valheim',
+  })
+  game.save().then(result => {
+    console.log('game saved!')
+    mongoose.connection.close()
+  })
 })
 
 app.post("/generate-short-description", async (request, response) => {
